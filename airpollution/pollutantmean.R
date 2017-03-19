251157473afad8554478f254cd5ff2d2db5dff1a
Gaurav Poothia <gaurav.poothia@nutanix.com>
2017-03-19 16:05:33 -0700

    Calculate mean of pollutant given station ids

---
+++ airpollution/pollutantmean.R
@@ -0,0 +1,22 @@
+pollutantmean <- function(directory, pollutant, id){
+  setwd(directory)
+  filenames <- system("ls .",intern=TRUE)
+  accu = vector()
+  for(i in 1:length(filenames)){
+    if (i %in% id) {
+      print(filenames[i])
+      df <- read.csv(filenames[i])
+      col <- df[[pollutant]]
+      good <- complete.cases(col)
+      filter_col = col[good]
+      # all NA col will result in col[good] being a logical
+      if (class(filter_col) != class(TRUE)) {
+        accu = c(accu, filter_col)
+      }
+    }
+  }
+  mean(accu)
+}
+
+setwd("/Users/gauravpoothia/code/repos/coursera-data-science/airpollution/")
+print(pollutantmean("specdata", "nitrate", 23))
